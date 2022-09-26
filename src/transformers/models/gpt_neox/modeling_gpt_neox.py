c086724e51814e9764f03c72bb9b7d14cac4e942
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2022-09-25 20:49:08 -0600

    Update modeling_gpt_neox.py

--- src/transformers/models/gpt_neox/modeling_gpt_neox.py
+++ src/transformers/models/gpt_neox/modeling_gpt_neox.py
@@ -290,75 +290,17 @@ class GPTNeoXMLP(nn.Module):
         return hidden_states
 
 
-## https://github.com/cmsflash/efficient-attention/blob/master/efficient_attention.py
-import torch
-from torch import nn
-from torch.nn import functional as f
-
-
-class EfficientAttention(nn.Module):
-
-    def __init__(self, in_channels, key_channels, head_count, value_channels):
-        super().__init__()
-        self.in_channels = in_channels
-        self.key_channels = key_channels
-        self.head_count = head_count
-        self.value_channels = value_channels
-
-        self.keys = nn.Conv2d(in_channels, key_channels, 1)
-        self.queries = nn.Conv2d(in_channels, key_channels, 1)
-        self.values = nn.Conv2d(in_channels, value_channels, 1)
-        self.reprojection = nn.Conv2d(value_channels, in_channels, 1)
-
-    def forward(self, input_):
-        n, h, w = input_.size()
-        keys = self.keys(input_).reshape((n, self.key_channels, h * w))
-        queries = self.queries(input_).reshape(n, self.key_channels, h * w)
-        values = self.values(input_).reshape((n, self.value_channels, h * w))
-        head_key_channels = self.key_channels // self.head_count
-        head_value_channels = self.value_channels // self.head_count
-
-        attended_values = []
-        for i in range(self.head_count):
-            key = f.softmax(keys[
-                            :,
-                            i * head_key_channels: (i + 1) * head_key_channels,
-                            :
-                            ], dim=2)
-            query = f.softmax(queries[
-                              :,
-                              i * head_key_channels: (i + 1) * head_key_channels,
-                              :
-                              ], dim=1)
-            value = values[
-                    :,
-                    i * head_value_channels: (i + 1) * head_value_channels,
-                    :
-                    ]
-            context = key @ value.transpose(1, 2)
-            attended_value = (
-                    context.transpose(1, 2) @ query
-            ).reshape(n, head_value_channels, h, w)
-            attended_values.append(attended_value)
-
-        aggregated_values = torch.cat(attended_values, dim=1)
-        reprojected_value = self.reprojection(aggregated_values)
-        attention = reprojected_value + input_
-
-        return attention
-
 class GPTNeoXLayer(nn.Module):
     def __init__(self, config):
         super().__init__()
         self.input_layernorm = nn.LayerNorm(config.hidden_size, eps=config.layer_norm_eps)
         self.post_attention_layernorm = nn.LayerNorm(config.hidden_size, eps=config.layer_norm_eps)
         # self.attention = BigBirdPegasusSelfAttention(config)
-        #self.attention = GPTNeoXAttention(config)
+        self.attention = GPTNeoXAttention(config)
         # self.attention = SparseSelfAttention(SparsityConfig(num_heads=config.num_attention_heads),
         #                                     max_seq_length=config.max_position_embeddings)
         # self.attention = LSHSelfAttention(config)
         # self.attention = LongformerSelfAttention(config, layer_id=0)
-        self.attention = EfficientAttention(in_channels=0, key_channels=0, head_count=config.num_attention_heads, value_channels=0)
         self.mlp = GPTNeoXMLP(config)
         self.use_deepspeed_checkpointing = config.use_deepspeed_checkpointing
 
