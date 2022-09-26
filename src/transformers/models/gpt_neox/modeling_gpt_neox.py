ef31559ca124b178993b77bdcc0ada408bdabc1e
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2022-09-25 22:50:52 -0600

    Update modeling_gpt_neox.py

--- src/transformers/models/gpt_neox/modeling_gpt_neox.py
+++ src/transformers/models/gpt_neox/modeling_gpt_neox.py
@@ -214,7 +214,7 @@ class GPTNeoXAttention(nn.Module):
 
         query = query.view(batch_size * num_attention_heads, query_length, attn_head_size)
         key = key.view(batch_size * num_attention_heads, key_length, attn_head_size)
-        attn_scores = opt_einsum_torch.einsum("bik,bjk->bij", query, key) / self.norm_factor
+        attn_scores = opt_einsum_torch.einsum("bik,bjk->bij", query, key, cuda_device=torch.cuda.current_device()) / self.norm_factor
         # attn_scores = oe.contract("bik,bjk->bij", query, key, backend="torch") / self.norm_factor
         attn_scores = attn_scores.view(batch_size, num_attention_heads, query_length, key_length)
 
