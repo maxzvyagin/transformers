72076db07d631cecb08bd61c2b7b1c74a84312ae
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2022-11-06 14:14:26 -0700

    cuda call

--- src/transformers/models/gpt_neox/modeling_gpt_neox.py
+++ src/transformers/models/gpt_neox/modeling_gpt_neox.py
@@ -207,7 +207,7 @@ class GPTNeoXAttention(nn.Module):
         query = query.view(batch_size * num_attention_heads, query_length, attn_head_size)
         key = key.view(batch_size * num_attention_heads, key_length, attn_head_size)
         # attn_scores = torch.einsum("bik,bjk->bij", query, key) / self.norm_factor
-        attn_scores = einsum("bik,bjk->bij", query, key) / self.norm_factor
+        attn_scores = einsum("bik,bjk->bij", query, key).cuda() / self.norm_factor
         attn_scores = attn_scores.view(batch_size, num_attention_heads, query_length, key_length)
 
         attn_scores = torch.where(causal_mask, attn_scores, self.masked_bias.to(attn_scores.dtype))
